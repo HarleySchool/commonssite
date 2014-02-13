456907a73de12cff3afc53183f62f72520fa4b8c
wrongu <richard.d.lange.13@dartmouth.edu>
2014-02-13 13:51:05 -0500

    added empty file for weather scraping wrapper

---
+++ scraper_weather.py
@@ -0,0 +1,10 @@
+from scraper_template import ScraperTemplate
+
+
+class ScraperWeather(ScraperTemplate):
+
+	def __init__(self):
+		ScraperTemplate.__init__("Weather Station")
+
+	def get_data():
+		return {}
