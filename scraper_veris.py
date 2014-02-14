8a4f7482c45d95c343c1c58f6f9ad8a02c5167da
wrongu <richard.d.lange.13@dartmouth.edu>
2014-02-14 14:06:41 -0500

    xml import in separate file; veris uses it

--- scraper_veris.py
+++ scraper_veris.py
@@ -1,40 +1,15 @@
+import requests
 from settings import veris_host, veris_port, veris_uname, veris_password
+from xml_import import etree
 
-# import xml parsing in a version-independent way
-try:
-	from lxml import etree
-	print("running with lxml.etree")
-except ImportError:
-	try:
-		# Python 2.5
-		import xml.etree.cElementTree as etree
-		print("running with cElementTree on Python 2.5+")
-	except ImportError:
-		try:
-			# Python 2.5
-			import xml.etree.ElementTree as etree
-			print("running with ElementTree on Python 2.5+")
-		except ImportError:
-			try:
-				# normal cElementTree install
-				import cElementTree as etree
-				print("running with cElementTree")
-			except ImportError:
-				try:
-					# normal ElementTree install
-					import elementtree.ElementTree as etree
-					print("running with ElementTree")
-				except ImportError:
-					print("Failed to import ElementTree from any known place")
-
-def get_data(channel):
-	full_url = "http://%s:%d/setup/devicexml.cgi?ADDRESS=%d&TYPE=DATA" % (veris_host, veri_port, channel)
+def get_xml_string(channel):
+	full_url = "http://%s:%d/setup/devicexml.cgi?ADDRESS=%d&TYPE=DATA" % (veris_host, veris_port, channel)
 	req = requests.get(full_url, auth=(veris_uname, veris_password))
-	return req.text
+	return str(req.text)
 
 if __name__ == '__main__':
-	xml_obj_1 = etree.fromstring(get_data(3))
-	xml_obj_2 = etree.fromstring(get_data(4))
+	xml_obj_1 = etree.fromstring(get_xml_string(3))
+	xml_obj_2 = etree.fromstring(get_xml_string(4))
 	print etree.tostring(xml_obj_1, pretty_print=True)
 	# TODO parse fully into dict
 	print xml_obj_1.find('./devices/device/name').text,
---
+++ xml_import.py
@@ -0,0 +1,31 @@
+# xml_import.py
+#
+# this file is a wrapper for importing ElementTree XML parsing, since the import is fairly messy
+
+# import xml parsing in a version-independent way
+
+try:
+	from lxml import etree
+	print("running with lxml.etree")
+except ImportError:
+	try:
+		# Python 2.5
+		import xml.etree.cElementTree as etree
+		print("running with cElementTree on Python 2.5+")
+	except ImportError:
+		try:
+			# Python 2.5
+			import xml.etree.ElementTree as etree
+			print("running with ElementTree on Python 2.5+")
+		except ImportError:
+			try:
+				# normal cElementTree install
+				import cElementTree as etree
+				print("running with cElementTree")
+			except ImportError:
+				try:
+					# normal ElementTree install
+					import elementtree.ElementTree as etree
+					print("running with ElementTree")
+				except ImportError:
+					print("Failed to import ElementTree from any known place")
