ebacf1dbd5783984505e90863007a0e510a48d22
wrongu <lange.richard.d@gmail.com>
2014-02-27 12:09:04 -0500

    user-independent path in run_migrators

--- run_migrators.py
+++ run_migrators.py
@@ -1,5 +1,6 @@
 # run migrators
 from commonssite.migrators import *
+from os.path import expanduser 
 
 if __name__ == '__main__':
-	HvacMigrator.migrate('/home/controlroom/code/hvac/log-snapshots/0206.csv')
+	HvacMigrator.migrate(expanduser('~/code/hvac/log-snapshots/0206.csv'))
