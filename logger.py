3a6aaa92028ae2cb3f24dffa33e937a9b16a4e11
wrongu <richard.d.lange.13@dartmouth.edu>
2014-02-27 09:40:21 -0500

    fixed tnext bug in logger

--- logger.py
+++ logger.py
@@ -9,25 +9,25 @@ HOUR = 60*MINUTE
 DAY = 24*HOUR
 WEEK = 7*DAY
 
-class HvacLogger(Thread):
+class Logger(Thread):
 
-	def __init__(self, loginterval, logspan=-1):
+	def __init__(self, scraper_obj, loginterval, logspan=-1):
 		Thread.__init__(self)
-		self.scraper = ScraperHvac()
+		self.scraper = scraper_obj
 		self.interval = loginterval
 		self.span = logspan
 
 	def run(self):
 		"""start logging in the background
 
-		this function is executed in a separate thread when obj.start() is called"""
+		this function is executed in a separate thread when Logger.start() is called"""
 		tstart = time.time()
 		tnext = tstart + self.interval
 		tend = tstart + self.span
 
 		def do_log():
 			now = datetime.datetime.now()
-			print "getting log at", now
+			print "------------------------\ngetting log at", now
 			for entry in self.scraper.get_data():
 				entry.save(force_insert=True)
 
@@ -35,14 +35,18 @@ class HvacLogger(Thread):
 			now = time.time()
 			tleft = max(0, tnext-now)
 			time.sleep(tleft)
+			return time.time()
 
 		# execution begins with a log entry
 		do_log()
 		# now wait for next one
 		while self.span < 0 or tnext < tend:
 			try:
-				sleep_remainder()
+				now = sleep_remainder()
 				do_log()
+				# update tnext to be the next integer multiple of 'interval' seconds
+				tnext = tstart + interval * (1 + math.floor((now - tstart) / interval))
+				print "next log scheduled for", datetime.fromtimestamp(tnext)
 			except KeyboardInterrupt:
 				print "exiting HVAC logger thread"
 				break
