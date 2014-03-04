772372ed60ca2d6beba61290990cac8c9702f066
wrongu <richard.d.lange.13@dartmouth.edu>
2014-03-04 14:17:20 -0500

    global static file storage

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -93,3 +93,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import os
+STATIC_ROOT = os.path.expanduser("~/code/commonssite/webapp/static/")
