6c05c731e40742e5243c68317ad69b2610e6471b
wrongu <richard.d.lange.13@dartmouth.edu>
2014-03-04 14:25:23 -0500

    made global static files directory. javascript moved there.

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -93,6 +93,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-
-import os
-STATIC_ROOT = os.path.expanduser("~/code/commonssite/webapp/static/")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, "static-collected/")
