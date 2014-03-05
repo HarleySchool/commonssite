d79a6a01292a0ef11c72c66017589e0d93c7e8e3
wrongu <richard.d.lange.13@dartmouth.edu>
2014-03-05 16:22:15 -0500

    landing page for webapp (base.html)

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic.base import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
+class IndexView(TemplateView):
+    template_name = "base.html"
+
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'webapp.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', IndexView.as_view()),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^data/', include('data.urls'))
 )
