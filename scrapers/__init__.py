8947dd104a57d7be439e55fbcc95dd3bc3d4df34
wrongu <lange.richard.d@gmail.com>
2014-03-11 11:40:58 -0400

    [unfinished] an attempt to make the Logger class part of the webapp

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -1,4 +1,5 @@
 from commonssite.scrapers.hvac import ScraperHvac
 from commonssite.scrapers.electric import ScraperElectric
+from commonssite.scrapers.logger import Logger
 
-__all__ = ['ScraperHvac', 'ScraperElectric']
+__all__ = ['ScraperHvac', 'ScraperElectric', 'Logger']
