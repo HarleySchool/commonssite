b27a88c00d5788308ae416485dcdd682ea0a3138
wrongu <richard.d.lange.13@dartmouth.edu>
2014-03-27 12:40:28 -0400

    moved hvac scraper to server app

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -1,4 +1,4 @@
-from commonssite.scrapers.hvac import ScraperERV, ScraperVRF
+from commonssite.server.hvac.scrapers import ScraperERV, ScraperVRF
 from commonssite.scrapers.sma import ScraperSMA
 from commonssite.scrapers.electric import ScraperElectric
 from commonssite.scrapers.logger import Logger
