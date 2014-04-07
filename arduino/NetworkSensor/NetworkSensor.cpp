0aeb3fb1635661c4228b6e707c18ad206e604533
wrongu <richard.d.lange.13@dartmouth.edu>
2014-04-07 13:51:43 -0400

    NTP handled by the NetworkSensor class itself. Still no sync, but very close!

--- arduino/NetworkSensor/NetworkSensor.cpp
+++ arduino/NetworkSensor/NetworkSensor.cpp
@@ -1,262 +1,189 @@
 #include "NetworkSensor.h"
 
-NetworkSensor::NetworkSensor() : initialized(false), server(SERVERPORT) {}
-
-NetworkSensor::NetworkSensor(uint8_t mac[], uint8_t ip[]) : initialized(true), server(SERVERPORT)
-{
-	// Serial connection for debugging
-	Serial.begin(9600);
-	Serial.println("Serial connection ready.");
-	
-	// SS (Slave Select) pins are handled by SDMode() and ethernetMode()
-	pinMode(ETHSS, OUTPUT);
-	pinMode(SDSS,  OUTPUT);
-
-	// initialize
-	Serial.println("connecting SD");
-	initSD();
-	Serial.println("connecting Ethernet");
-	initEthernet(mac, ip);
-
-	// set up clock (important that it is after initEthernet())
-	this->clock = TimeNTP(NTP_SYNC);
-}
+NetworkSensor::NetworkSensor()
+: initialized(false), server(SERVERPORT), last_time_sync(0UL), time_sync_interval(NTP_SYNC_MINUTES*60000)
+{}
 
 NetworkSensor::~NetworkSensor(){}
 
-void NetworkSensor::serve()
+void NetworkSensor::begin(uint8_t mac[], uint8_t ip[])
 {
-	if(!initialized){
-		Serial.println("ERROR: attempting to used uninitialized NetworkSensor (did you forget 'sensor=NetworkSensor(mac, ip)' ??");
-		return;
-	}
-	ethernetMode();
-	// make sure clock is synchronized
-	this->clock.checkSync();
-	// check for client connections and write requested data back
-	EthernetClient client = server.available();
-	if (client) {
-		// an http request ends with a blank line
-		boolean currentLineIsBlank = true;
-		while (client.connected()) {
-			if (client.available()) {
-				char c = client.read();
-				// if we've gotten to the end of the line (received a newline
-				// character) and the line is blank, the http request has ended,
-				// so we can send a reply
-				if (c == '\n' && currentLineIsBlank) {
-					// send a standard http response header
-					client.println("HTTP/1.1 200 OK");
-					client.println("Content-Type: application/json");
-					client.println();
+  // Serial connection for debugging
+  Serial.begin(9600);
+  
+  // SS (Slave Select) pins are handled by SDMode() and ethernetMode()
+  pinMode(ETHSS, OUTPUT);
+  pinMode(SDSS,  OUTPUT);
 
-					String content("NO CONTENT YET, SORRY");
-					client.println("{");
-					{
-						// the format of the file is lines of 'Name : value,', so
-						// no further processing is needed to make it json-style
-						client.println(content);
-					}
-					client.println("}");
+  initSD();
+  initEthernet(mac, ip);
 
-					// for debugging
-					Serial.println(content);
-					
-					break;
-				}
-				currentLineIsBlank = (c == '\n' || c == '\r');
-			}
-		}
-		delay(1);
-		client.stop();
-	}
+  // set up time synchronization
+  syncTimeNTP();
 }
 
-unsigned long NetworkSensor::getTime()
+void NetworkSensor::serve()
 {
-	return this->clock.now();
+  if(!initialized){
+    return;
+  }
+  ethernetMode();
+  // check if it's time to re-sync the clock
+  if((millis() - last_time_sync) > time_sync_interval){
+    syncTimeNTP();
+  }
+  // check for client connections and write requested data back
+  EthernetClient client = server.available();
+  if (client) {
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        // if we've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so we can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: application/json");
+          client.println();
+
+          String content("NO CONTENT YET, SORRY");
+          client.println("{");
+          {
+            // the format of the file is lines of 'Name : value,', so
+            // no further processing is needed to make it json-style
+            client.println(content);
+          }
+          client.println("}");
+
+          // for debugging
+          //Serial.println(content);
+          
+          break;
+        }
+        currentLineIsBlank = (c == '\n' || c == '\r');
+      }
+    }
+    delay(1);
+    client.stop();
+  }
 }
 
 void NetworkSensor::logf(String name, float value, unsigned int precision)
 {
-	log(name, floatToString(value, precision));
+  log(name, floatToString(value, precision));
 }
 
 void NetworkSensor::logi(String name, int value)
 {
-	log(name, String(value));
+  log(name, String(value));
 }
 
 void NetworkSensor::logs(String name, String value)
 {
-	String quoted_value = "\"" + value + "\"";
-	log(name, quoted_value);
+  String quoted_value = "\"" + value + "\"";
+  log(name, quoted_value);
 }
 
 void NetworkSensor::initSD()
 {
-	// SD pin configuration
-	SDMode();
-	// see if the card is present and can be initialized:
-	if (SD.begin(SDSS)) {
-		Serial.println("SD card initialized.");
-	} else{
-		initialized = false;
-		Serial.println("failed, or card not plugged in");
-	}
+  // SD pin configuration
+  SDMode();
+  // see if the card is present and can be initialized:
+  if (SD.begin(SDSS)) {
+    //Serial.println("SD card initialized.");
+  } else{
+    initialized = false;
+    //Serial.println("failed, or card not plugged in");
+  }
 }
 
 void NetworkSensor::initEthernet(uint8_t mac[], uint8_t ip[])
 {
-	// Server initialization
-	ethernetMode();
-	IPAddress ip_addr(ip[0], ip[1], ip[2], ip[3]);
-	Ethernet.begin(mac, ip_addr);
-	Serial.print("Opened server connection on ");
-	Serial.print(ip[0]); Serial.print("."); Serial.print(ip[1]); Serial.print("."); Serial.print(ip[2]); Serial.print("."); Serial.println(ip[3]);
-	server.begin();
+  // Server initialization
+  ethernetMode();
+  IPAddress ip_addr(ip[0], ip[1], ip[2], ip[3]);
+  Ethernet.begin(mac, ip_addr);
+  ////Serial.print("Opened server connection on ");
+  ////Serial.print(ip[0]); //Serial.print("."); //Serial.print(ip[1]); //Serial.print("."); //Serial.print(ip[2]); //Serial.print("."); //Serial.println(ip[3]);
+  server.begin();
 }
 
 void NetworkSensor::log(String name, String stringifiedValue)
 {
-	if(!initialized){
-		Serial.println("ERROR: attempting to used uninitialized NetworkSensor (did you forget 'sensor=NetworkSensor(mac, ip)' ??");
-		return;
-	}
-	SDMode();
-	// TODO
+  if(!initialized){
+    return;
+  }
+  SDMode();
+  // TODO
 }
 
-// ---------------
-// -- NTP Clock --
-// ---------------
-
-
-TimeNTP::TimeNTP() : last_update(0L), sync_period(0L)
-{}
-
-TimeNTP::TimeNTP(unsigned long sync_period_minutes)
-: last_update(0L), sync_period(sync_period_minutes * 60000L)
+void NetworkSensor::syncTimeNTP()
 {
-	// based on a query from pool.ntp.org
-	// NOTE that this may become obsolete. The correct way to do it
-	// would be to do our own DNS on pool.ntp.org, but that is too
-	// complicated for now.
-	ip = IPAddress(166, 70, 136, 41);
-	Udp.begin(UDP_PORT);
-}
-
-void TimeNTP::checkSync(bool force_update, int max_retries)
-{
-	// check if update needed
-	if(force_update || (millis() - last_update) > sync_period){
-		unsigned long epochTime = 0L;
-		int tries = 0;
-		while(epochTime == 0L && tries++ < max_retries){
-			Serial.println("syncing NTP");
-			sendNTPpacket(this->ip);
-			delay(1000);
-			epochTime = readResponseAsEpoch();
-		}
-		// update if successful
-		if(epochTime != 0L){
-			last_update = millis();
-			setTime(epochTime);
-			Serial.print("Time synced! it is now ");
-			Serial.print(this->month());
-			Serial.print("/");
-			Serial.print(this->day());
-			Serial.print("/");
-			Serial.print(this->year());
-			Serial.print(" ");
-			Serial.print(this->hour());
-			Serial.print(":");
-			Serial.print(this->minute());
-			Serial.print(":");
-			Serial.println(this->second());
-		} else{
-			Serial.println("Sync failed");
-		}
-	}
-}
-
-unsigned long TimeNTP::now()
-{
-	if(last_update > 0L) return now();
-	else return 0L;
-}
-
-unsigned int TimeNTP::year()
-{
-	if(last_update > 0L) return year();
-	else return 0L;
-}
-
-unsigned int TimeNTP::month()
-{
-	if(last_update > 0L) return month();
-	else return 0L;
-}
-
-unsigned int TimeNTP::day(){
-	if(last_update > 0L) return day();
-	else return 0L;
-}
-
-unsigned int TimeNTP::hour(){
-	if(last_update > 0L) return hour();
-	else return 0L;
-}
-
-unsigned int TimeNTP::minute(){
-	if(last_update > 0L) return minute();
-	else return 0L;
-}
-
-unsigned int TimeNTP::second(){
-	if(last_update > 0L) return second();
-	else return 0L;
-}
-
-void TimeNTP::sendNTPpacket(IPAddress& address)
-{
-	// consume and discard all existing requests
-	while(Udp.parsePacket() > 0);
-	// PROTOCOL SPECIFICATION: http://tools.ietf.org/html/rfc958#appendix-A
-	// (or see the Time>TimeNTP example that comes with Arduino)
-	// create byte buffer
-	byte packetBuffer[NTP_PACKET_SIZE];
-	// clear buffer to zeros
-	memset(packetBuffer, 0, NTP_PACKET_SIZE);
-	// set packet data as an NTP request
-	packetBuffer[0]  = 0b11100011;
-	packetBuffer[1]  = 0;
-	packetBuffer[2]  = 0xEC;
-	packetBuffer[3]  = 6;
-	packetBuffer[12] = 49;
-	packetBuffer[13] = 0x4E;
-	packetBuffer[14] = 49;
-	packetBuffer[15] = 52;
-	Udp.beginPacket(address, NTP_PORT);
-	Udp.write(packetBuffer, NTP_PACKET_SIZE);
-	Udp.endPacket();
-}
-
-unsigned long TimeNTP::readResponseAsEpoch()
-{
-	// PROTOCOL SPECIFICATION: http://tools.ietf.org/html/rfc958#appendix-B
-	if(Udp.parsePacket() >= NTP_PACKET_SIZE){
-		byte packetBuffer[NTP_PACKET_SIZE];
-		Udp.read(packetBuffer, NTP_PACKET_SIZE);
-		unsigned long highWord, lowWord, epoch;
-		highWord = word(packetBuffer[40], packetBuffer[41]);
-		lowWord = word(packetBuffer[42], packetBuffer[43]);
-		epoch = highWord << 16 | lowWord;
-		// NTP specifies the # of seconds since 1900.
-		// Unix time is since 1970.
-		// There are 2208988800 seconds between 1900 and 1970  
-		epoch -= 2208988800UL;
-		return epoch;
-	}
-	return 0L;
+  // to begin, shut down the server to free sockets
+  // (ok because this function is called very infrequently and doesn't take
+  // a significant amount of time)
+  server.available().stop();
+  {
+    EthernetUDP Udp;
+    Udp.begin(8888);
+    IPAddress ntpServer(132, 163, 4, 101);
+    const int PACKET_SIZE = 48;
+    byte packetBuffer[PACKET_SIZE];
+    /////////////////////
+    // SEND UDP PACKET //
+    /////////////////////
+    Serial.println("Sending NTP packet");
+    while (Udp.parsePacket() > 0) ; // discard any previously received packets
+    // set all bytes in the buffer to 0
+    memset(packetBuffer, 0, PACKET_SIZE);
+    // Initialize values needed to form NTP request
+    // (see URL above for details on the packets)
+    packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+    packetBuffer[1] = 0;     // Stratum, or type of clock
+    packetBuffer[2] = 6;     // Polling Interval
+    packetBuffer[3] = 0xEC;  // Peer Clock Precision
+    // 8 bytes of zero for Root Delay & Root Dispersion
+    packetBuffer[12]  = 49;
+    packetBuffer[13]  = 0x4E;
+    packetBuffer[14]  = 49;
+    packetBuffer[15]  = 52;
+    // all NTP fields have been given values, now
+    // you can send a packet requesting a timestamp:                 
+    Udp.beginPacket(ntpServer, 123); // NTP protocol uses port 123
+    Udp.write(packetBuffer, PACKET_SIZE);
+    Udp.endPacket();
+    ///////////////////
+    // READ RESPONSE // 
+    ///////////////////
+    Serial.println("Reading response");
+    unsigned long beginWait = millis();
+    // wait a full 1500 milliseconds before giving up on response
+    while (millis() - beginWait < 1500UL) {
+      int size = Udp.parsePacket();
+      if (size >= PACKET_SIZE) {
+        Serial.println("Received NTP Data");
+        Udp.read(packetBuffer, PACKET_SIZE);  // read packet into the buffer
+        unsigned long secsSince1900;
+        // convert four bytes starting at location 40 to a long integer
+        secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+        secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+        secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+        secsSince1900 |= (unsigned long)packetBuffer[43];
+        // NTP measures time in seconds since 1900
+        // Unix measures time in seconds since 1970
+        // There are 2208988800 seconds between 1900 and 1970.
+        unsigned long epoch = secsSince1900 - 2208988800UL;
+        setTime(epoch);
+        last_time_sync = millis();
+      } else{
+        //Serial.print("packet size: ");
+        //Serial.println(size);
+      }
+    }
+  }
+  //Serial.println("NTP transaction done!");
+  server.begin();
 }
