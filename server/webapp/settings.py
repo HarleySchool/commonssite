068ba3389211c023f65aa8afe91b70e13a2f1983
MB6 <max.bednarcyk@gmail.com>
2014-04-23 11:12:59 -0400

    progress

--- server/webapp/settings.py
+++ server/webapp/settings.py
@@ -85,7 +85,6 @@ DATABASES = {
 'ENGINE': 'django.db.backends.sqlite3',
 'NAME': os.path.expanduser('~/code/commonssite/mydatabase'),
 }
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
