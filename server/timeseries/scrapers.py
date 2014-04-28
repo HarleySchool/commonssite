86a647a9298f4b598a59a82ab33509c5cacee20b
wrongu <richard.d.lange.13@dartmouth.edu>
2014-04-28 13:40:14 -0400

    updated ScraperBase with get_and_save_single

--- server/timeseries/scrapers.py
+++ server/timeseries/scrapers.py
@@ -1,3 +1,4 @@
+import time
 
 class ScraperBase(object):
 	"""A scraper base class which takes care of shared functionality
@@ -10,6 +11,17 @@ class ScraperBase(object):
 		latest = cls.objects.order_by('-Time')[0]
 		latest.temporary = False
 		latest.save() # performs a SQL UPDATE
+
+	@classmethod
+	def name(cls):
+		return cls.__name__
+
+	def get_and_save_single(self):
+		for new_data in self.get_data():
+			new_data.temporary = True
+			new_data.save(force_insert=True)
+		print '=================='
+		print '%s done at %s' % (self.name(), time.time())
 
 	def get_data():
 		"""Return a list of TimeseriesBase (subclass) models
