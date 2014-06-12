d435b1331d9bfef23c18c6080bdebc3f491c3451
wrongu <lange.richard.d@gmail.com>
2014-06-12 13:31:57 -0400

    bugfix; needed an import for live page

--- server/timeseries/views/__init__.py
+++ server/timeseries/views/__init__.py
@@ -2,6 +2,7 @@ from data_api import systems, series, single, save_series, load_series
 from data_analysis import download_csv, analyze
 from admin_pages import status
 import timeseries.helpers as h
+from django.shortcuts import render
 
 def live(request):
 	return render(request, 'timeseries/live.html', {'systems' : h.systems_schema()})
