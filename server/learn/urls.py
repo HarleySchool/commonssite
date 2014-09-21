0ff9a947b9b8bd2b4798b03124902e67a862a161
wrongu <lange.richard.d@gmail.com>
2014-09-21 19:08:37 -0400

    fixed solarwater directing to solar

--- server/learn/urls.py
+++ server/learn/urls.py
@@ -3,11 +3,11 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
 	url(r'^$',          TemplateView.as_view(template_name="learn/overview.html")),
-	url(r'^electric',   TemplateView.as_view(template_name="learn/electric.html")),
-	url(r'^solar',      TemplateView.as_view(template_name="learn/solar.html")),
-	url(r'^greenhouse', TemplateView.as_view(template_name="learn/greenhouse.html")),
-	url(r'^hvac',       TemplateView.as_view(template_name="learn/hvac.html")),
-	url(r'^geothermal', TemplateView.as_view(template_name="learn/geothermal.html")),
-	url(r'^solarwater', TemplateView.as_view(template_name="learn/solarwater.html")),
-	url(r'^weather',    TemplateView.as_view(template_name="learn/weather.html")),
+	url(r'^electric/?$',   TemplateView.as_view(template_name="learn/electric.html")),
+	url(r'^solar/?$',      TemplateView.as_view(template_name="learn/solar.html")),
+	url(r'^greenhouse/?$', TemplateView.as_view(template_name="learn/greenhouse.html")),
+	url(r'^hvac/?$',       TemplateView.as_view(template_name="learn/hvac.html")),
+	url(r'^geothermal/?$', TemplateView.as_view(template_name="learn/geothermal.html")),
+	url(r'^solarwater/?$', TemplateView.as_view(template_name="learn/solarwater.html")),
+	url(r'^weather/?$',    TemplateView.as_view(template_name="learn/weather.html")),
 )
